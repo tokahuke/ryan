b4db87f066363a8779983b6c9d420d489e014c53
Pedro Arruda <parruda@findhotel.net>
2023-02-04 20:49:38 -0300

    ryan online improvements

--- ryan-js/ryan-online/src/index.js
+++ ryan-js/ryan-online/src/index.js
@@ -7,16 +7,31 @@ import "brace/theme/monokai";
 
 window.ryan = ryan;
 
+const sampleProgram = `// This is some sample Ryan code
+// Edit at will!
+
+let number_of_lights = 4;
+
+{
+    picard: number_of_lights,
+    gul_madred: number_of_lights + 1,
+}
+`;
+const initialCode = new URLSearchParams(location.search).get("c") || sampleProgram;
+document.querySelector("#editor-area").textContent = initialCode;
+
 const editor = ace.edit("editor-area");
-editor.getSession().setMode('ace/mode/javascript');
+editor.setShowPrintMargin(false);
 editor.setTheme('ace/theme/monokai');
+editor.getSession().setMode('ace/mode/javascript');
 editor.getSession().setUseWorker(false);
 editor.getSession().setUseWrapMode(true);
 
 const display = ace.edit("display-area");
-display.getSession().setMode('ace/mode/json');
+display.setShowPrintMargin(false);
 display.setTheme('ace/theme/monokai');
 display.setReadOnly(true);
+display.getSession().setMode('ace/mode/json');
 display.getSession().setUseWrapMode(true);
 
 const runCycle = () => {
@@ -34,3 +49,18 @@ const runCycle = () => {
 
 runCycle();
 editor.on("change", () => runCycle());
+
+
+document.querySelector("#share-button").addEventListener("click", async (_e) => {
+    const params = new URLSearchParams(location.search);
+    params.set("c", editor.getValue());
+    const generatedLink = `${location.href.split('?')[0]}?${params.toString()}`;
+
+    navigator.clipboard.writeText(generatedLink);
+    document.querySelector("#share-button .icon-desc").textContent = "COPIED!";
+
+    await new Promise(resolve => setTimeout(resolve, 1_000));
+
+    console.log("Hello!");
+    document.querySelector("#share-button .icon-desc").textContent = "COPY LINK";
+});
